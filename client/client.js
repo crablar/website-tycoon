16dfdce30d08f3c50e2e5d5b07e7d369822c6e64
crablar <meff.jeyerson@gmail.com>
2015-04-24 09:24:48 -0700

    Content is publishable and unpublishable

--- client/client.js
+++ client/client.js
@@ -4,15 +4,22 @@ Meteor.subscribe('statistics');
 
   console.log(Date.now());
 
-  Template.list.getCards = function() {
-    return Cards.find(
-      {}
-    );
+  Template.list.getCards = function(isListPublished) {
+    return Cards.find({is_published : isListPublished});
   };
 
   Template.board.getLists = function(){
     return Lists.find({}, {sort: {order: 1}})
   };
+
+  Template.card.events({
+    'click .publish': function(){
+      Cards.update({_id : this._id}, {$set: {is_published : true}});
+    },
+    'click .unpublish': function(){
+      Cards.update({_id : this._id}, {$set: {is_published : false}});
+    }
+  });
 
   Template.login.events({
     'click #facebook-login': function(event) {
